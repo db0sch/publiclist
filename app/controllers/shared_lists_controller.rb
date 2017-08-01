9bed0fe9475d3d7a8a44feea693c1321495be82a
Dimitri Bosch <dimitribosch@hotmail.com>
2017-08-01 11:15:25 +0900

    Shared list layout for mobile and desktop

--- app/controllers/shared_lists_controller.rb
+++ app/controllers/shared_lists_controller.rb
@@ -2,9 +2,9 @@ class SharedListsController < ApplicationController
   def show
     @list = List.find_by(token: params[:token])
     if @list
-      wunderlist_service = ListsFromWunderlistService.new(current_user)
-      wunderlist_list = wunderlist_service.fetch_for_id(@list.list_id)
-      @tasks = wunderlist_list.tasks
+      # wunderlist_service = ListsFromWunderlistService.new(current_user)
+      # wunderlist_list = wunderlist_service.fetch_for_id(@list.list_id)
+      # @tasks = wunderlist_list.tasks
     else
       render :not_found unless @list
     end
