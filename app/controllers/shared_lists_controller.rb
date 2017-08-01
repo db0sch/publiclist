f70d2db863583dd7580e6e6d6cafd37855c2fa47
Dimitri Bosch <dimitribosch@hotmail.com>
2017-08-01 16:23:15 +0900

    Shared list css colors and font

--- app/controllers/shared_lists_controller.rb
+++ app/controllers/shared_lists_controller.rb
@@ -1,10 +1,12 @@
 class SharedListsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
   def show
     @list = List.find_by(token: params[:token])
     if @list
-      # wunderlist_service = ListsFromWunderlistService.new(current_user)
-      # wunderlist_list = wunderlist_service.fetch_for_id(@list.list_id)
-      # @tasks = wunderlist_list.tasks
+      wunderlist_service = ListsFromWunderlistService.new(@list.user)
+      wunderlist_list = wunderlist_service.fetch_for_id(@list.list_id)
+      @tasks = wunderlist_list.tasks
     else
       render :not_found unless @list
     end
